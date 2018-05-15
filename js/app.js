1ddd9815dec3ef4bbba7394c53736413711f2841
Megan Boley <boleym11@gmail.com>
2018-05-15 16:35:16 -0400

    Added cards in array; modified shuffle fcn

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 /*
  * Create a list that holds all of your cards
  */
+/* link for animation https://daneden.github.io/animate.css/*/
 
+let card = document.querySelectorAll('.card');
+let cardDeck = Array.from(card);
 
 /*
  * Display the cards on the page
@@ -11,18 +14,18 @@
  */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
+function shuffle(cardDeck) {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
+        temporaryValue = cardDeck[currentIndex];
+        cardDeck[currentIndex] = cardDeck[randomIndex];
+        cardDeckrandomIndex] = temporaryValue;
     }
 
-    return array;
+    return cardDeck;
 }
 
 
@@ -36,3 +39,28 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+ /*increment move counter
+let moves=0;
+function moveCounter() {
+    moves=moves+1;
+    alert(moves);
+ }
+
+$('.card').click(function moveCounter());
+
+ /* timer function that starts with click on card*/
+
+/*const minutes = document.querySelector("#minutes")
+const seconds = document.querySelector("#seconds")
+let count = 0;
+
+function beginTimer = () => {
+  count += 1;
+  minutes.innerHTML = Math.floor(count / 60).toString().padStart(2, "0");
+  seconds.innerHTML = (count % 60).toString().padStart(2, "0");
+  }
+
+const timer = setInterval(beginTimer, 1000)
+
+$('.card').click(function beginTimer);
